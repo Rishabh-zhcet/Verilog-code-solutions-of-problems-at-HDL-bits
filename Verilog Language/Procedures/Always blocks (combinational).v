d5c92c67b21e256379eadae915ba6b10e5889083
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-08 16:06:28 +0530

    Create Always blocks (combinational).v

---
+++ Verilog Language/Procedures/Always blocks (combinational).v
@@ -0,0 +1,20 @@
+/*
+Problem Statement: Build an AND gate using both an assign statement and a combinational always block.
+                   (Since assign statements and combinational always blocks function identically, there is no way to enforce that you're using both methods. 
+                   But you're here for practice, right?...)
+*/
+
+//Solution:
+
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign= a && b;
+    always@(*)
+        out_alwaysblock=a && b;
+
+endmodule
