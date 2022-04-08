e544f8f11d154ade055d66734ead97eba5829f2b
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-08 15:34:26 +0530

    Create Vector reverasal 1.v

---
+++ Verilog Language/Vectors/Vector reverasal 1.v
@@ -0,0 +1,13 @@
+/*
+Problem Statement: Given an 8-bit input vector [7:0], reverse its bit ordering.
+*/
+
+//Solution:
+
+module top_module( 
+    input [7:0] in,
+    output [7:0] out
+);
+    assign out={in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
+
+endmodule
