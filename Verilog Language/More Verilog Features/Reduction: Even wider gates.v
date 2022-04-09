0dac4c9d3d9f8a09d804a14546240caa11c2307c
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-09 18:22:29 +0530

    Create Reduction: Even wider gates.v

---
+++ Verilog Language/More Verilog Features/Reduction: Even wider gates.v
@@ -0,0 +1,23 @@
+/*
+Problem Statement: Build a combinational circuit with 100 inputs, in[99:0].
+
+                    There are 3 outputs:
+
+                    out_and: output of a 100-input AND gate.
+                    out_or: output of a 100-input OR gate.
+                    out_xor: output of a 100-input XOR gate.
+*/
+
+//Solution:
+
+module top_module( 
+    input [99:0] in,
+    output out_and,
+    output out_or,
+    output out_xor 
+);
+    assign out_and= &(in);
+    assign out_or= |(in);
+    assign out_xor=^(in);
+
+endmodule
