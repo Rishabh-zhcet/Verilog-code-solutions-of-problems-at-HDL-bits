f9395b06ce89db731f0e6c10b4e3eebc2136bd7d
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-09 18:24:10 +0530

    Create Combination for loop: Vector reversal 2.v

---
+++ Verilog Language/More Verilog Features/Combination for loop: Vector reversal 2.v
@@ -0,0 +1,20 @@
+/*
+Problem Statement: Given a 100-bit input vector [99:0], reverse its bit ordering.
+*/
+
+//Solution:
+
+module top_module( 
+    input [99:0] in,
+    output reg [99:0] out
+);
+    integer i;
+    always@(*)
+        begin
+            for( i=0; i<100;i=i+1)
+                begin
+                out[i]=in[99-i];
+                end
+        end
+ 
+endmodule
