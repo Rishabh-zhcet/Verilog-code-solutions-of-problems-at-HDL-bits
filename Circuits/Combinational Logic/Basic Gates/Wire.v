e87cb177ba57f8d47140c821ffe942199825c031
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-17 20:03:09 +0530

    Create Wire.v

---
+++ Circuits/Combinational Logic/Basic Gates/Wire.v
@@ -0,0 +1,16 @@
+/*
+Problem Statement: Implement the following circuit:
+                    in--------> out
+                    
+Solution:
+
+*/
+
+module top_module (
+    input in,
+    output out);
+    
+    assign out=in;
+
+endmodule
+
