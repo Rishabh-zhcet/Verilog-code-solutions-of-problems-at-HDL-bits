413e68e4b598bb90d3c7571c68a5451d9641f8e7
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-17 22:12:05 +0530

    Update Two-bit equality.v

--- Circuits/Combinational Logic/Basic Gates/Two-bit equality.v
+++ Circuits/Combinational Logic/Basic Gates/Two-bit equality.v
@@ -4,6 +4,9 @@ Problem Statement: Taken from 2015 midterm question 1k
                     The value of z should be 1 if A = B, otherwise z should be 0.
 Solution:
 */
+/*-----------------------------------------------------------------------------*/
+/*                                  My solution                                */
+/*-----------------------------------------------------------------------------*/
 
 module top_module ( input [1:0] A, input [1:0] B, output reg z ); 
 
@@ -15,4 +18,20 @@ module top_module ( input [1:0] A, input [1:0] B, output reg z );
                 z=1'b0;
         end
     
+endmodule
+
+/*-----------------------------------------------------------------------------*/
+/*                            HDL bits solution                                */
+/*-----------------------------------------------------------------------------*/
+
+module top_module(
+	input [1:0] A,
+	input [1:0] B,
+	output z);
+
+	assign z = (A[1:0]==B[1:0]);	// Comparisons produce a 1 or 0 result.
+	
+	// Another option is to use a 16-entry truth table ( {A,B} is 4 bits, with 16 combinations ).
+	// There are 4 rows with a 1 result.  0000, 0101, 1010, and 1111.
+
 endmodule
