2985dfc517cb2cd1f8d8daa299f3139916863897
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-17 21:59:14 +0530

    Update Truth_table.v

--- Verilog Language/Basics/Truth_table.v
+++ Verilog Language/Basics/Truth_table.v
@@ -17,6 +17,9 @@ Problem Statement: Create a combinational circuit that implements the below trut
                   
 Solution:
 */
+/*-----------------------------------------------------------------------------*/
+/*                                  My solution                                */
+/*-----------------------------------------------------------------------------*/
 
 module top_module( 
     input x3,
@@ -40,5 +43,28 @@ module top_module(
         endcase
         end
         
+endmodule
 
+/*-----------------------------------------------------------------------------*/
+/*                            HDL bits solution                                */
+/*-----------------------------------------------------------------------------*/
+
+module top_module (
+	input x3,
+	input x2,
+	input x1,
+	output f
+);
+	// This truth table has four minterms. 
+	assign f = ( ~x3 & x2 & ~x1 ) | 
+				( ~x3 & x2 & x1 ) |
+				( x3 & ~x2 & x1 ) |
+				( x3 & x2 & x1 ) ;
+				
+	// It can be simplified, by boolean algebra or Karnaugh maps.
+	// assign f = (~x3 & x2) | (x3 & x1);
+	
+	// You may then notice that this is actually a 2-to-1 mux, selected by x3:
+	// assign f = x3 ? x1 : x2;
+	
 endmodule
