d6c448c589968e3abceb7f40119ecbcf101fd261
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-17 20:04:52 +0530

    Create GND.v

---
+++ Circuits/Combinational Logic/Basic Gates/GND.v
@@ -0,0 +1,11 @@
+/*
+Problem statement: Implement the following circuit:
+                    output always ground 
+                    
+Solution:
+*/
+
+module top_module (
+    output out);
+assign out= 1'b0;
+endmodule
