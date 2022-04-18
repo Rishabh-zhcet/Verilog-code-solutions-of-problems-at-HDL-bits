513debc35e31199de55bc491bfaf41f1594e63f7
Rishabh Verma <vrishabh70086@gmail.com>
2022-04-18 20:21:55 +0530

    Update 7. Priority encoder with casez.v

--- Verilog Language/Procedures/7. Priority encoder with casez.v
+++ Verilog Language/Procedures/7. Priority encoder with casez.v
@@ -1,5 +1,7 @@
 /*
-Problem Statement: Create a module with one input and one output that behaves like a wire.
+Problem Statement: Build a priority encoder for 8-bit inputs. Given an 8-bit vector, the output should report the first bit in the vector that is 1. 
+                    Report zero if the input vector has no bits that are high.
+                    For example, the input 8'b10010000 should output 3'd4, because bit[4] is first bit that is high.
 */
 
 //Solution:
